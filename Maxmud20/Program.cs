78f23608f2c1e93d9045af374d6b76602613ab69
Makhmud <bmahmudbey@gmail.com>
2026-04-07 16:03:13 +0500

    19-20

---
+++ Maxmud20/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Maxmud20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Salom();
+            Salom("C#");
+            Salom();
+            Salom("Aziz");
+            Salom("Ali", 2501);
+        }
+        static void Salom() // parametrsiz
+        {
+            Console.WriteLine("Salom, metod!");
+        }
+        static void Salom(string ism) // 1 ta parametrli
+        {
+            Console.WriteLine($"Salom, {ism}!");
+        }
+        static void Salom(string ism, int gr) // 2 ta parametrli
+        {
+            Console.WriteLine($"Salom, {ism}!\nTMI-{gr} guruhi talabasi");
+        }
+    }
+}
